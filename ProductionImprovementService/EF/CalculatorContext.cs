cf942008f676c02696ff16005fdbc2d004316e79
M-Peplin <m.p.peplinski@gmail.com>
2021-02-25 20:53:05 +0100

    Created models

---
+++ ProductionImprovementService/EF/CalculatorContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ProductionImprovementService.Models;
+
+
+namespace ProductionImprovementService.EF
+{
+    public class CalculatorContext : DbContext
+    {
+        public CalculatorContext() : base("name=ConnectionString") { }
+
+        public DbSet<SearchHistory> SearchHistory { get; set; }
+        public DbSet<Module> Module { get; set; }
+        public DbSet<City> City { get; set; }
+    }
+}

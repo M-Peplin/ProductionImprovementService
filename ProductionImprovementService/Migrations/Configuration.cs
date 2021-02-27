bc2efbd379896c52a525ec12a680b52527b6c120
M-Peplin <m.p.peplinski@gmail.com>
2021-02-27 22:15:44 +0100

    Set connection string. Added first migration.

---
+++ ProductionImprovementService/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ProductionImprovementService.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProductionImprovementService.EF.CalculatorContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ProductionImprovementService.EF.CalculatorContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}

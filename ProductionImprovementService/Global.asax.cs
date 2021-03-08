990ee02918f50c3f9bda7fd8fcfba78068d4eceb
M-Peplin <m.p.peplinski@gmail.com>
2021-03-08 20:43:51 +0100

    Added injector. Registered injection services.

--- ProductionImprovementService/Global.asax.cs
+++ ProductionImprovementService/Global.asax.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using ProductionImprovementService.EF;
+using SimpleInjector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Http;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ProductionImprovementService.Services.Interfaces;
+using ProductionImprovementService.Services.Implementations;
+using SimpleInjector.Integration.WebApi;
 
 namespace ProductionImprovementService
 {
@@ -16,6 +22,19 @@ namespace ProductionImprovementService
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var container = new Container();
+            container.Register<CalculatorContext>(Lifestyle.Scoped);
+            container.Register<ICityService, CityService>(Lifestyle.Scoped);
+            container.Register<IModuleService, ModuleService>(Lifestyle.Scoped);
+            container.Register<ICalculatorCostService, CalculatorCostService>(Lifestyle.Scoped);
+            container.Register<ISearchHistoryService, SearchHistoryService>(Lifestyle.Scoped);
+            container.Register<IShowResultService, ShowResultService>(Lifestyle.Scoped);
+            container.Verify();
+
+            GlobalConfiguration.Configuration.DependencyResolver =
+                new SimpleInjectorWebApiDependencyResolver(container);
+            AreaRegistration.RegisterAllAreas();
         }
     }
 }
